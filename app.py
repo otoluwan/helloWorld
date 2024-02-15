e1b5dfcf38d6963d55cd147d57546e6f5bc22ad9
oreoluwatoluwani <otoluwan@terpmail.umd.edu>
2024-02-14 21:37:23 -0500

    Commiting new html page for the about page

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Oreoluwa Toluwani! I am adding my first code change'
+    return 'Hello World from Oreoluwa Toluwani! I am adding my first code change.'
 
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
-
+@app.route('/about')
+def hello():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run()
