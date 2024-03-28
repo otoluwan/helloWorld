2e0aab20bf776d77e5dc9c4ee73600cc4e7069a2
oreoluwatoluwani <otoluwan@terpmail.umd.edu>
2024-03-28 00:54:08 -0400

    added contact and favorite courses routes and included the contact form with 4 fields. Still working on the thank you message.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,5 +16,16 @@ def about():
 @app.route('/about-css')
 def aboutcss():
     return render_template('about-css.html')
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favoritecourse():
+    print(f'subject: {request.args.get("subject")}')
+    print(f'course_number: {request.args.get("course_number")}')
+    return render_template('favorite-course.html')
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted = True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
